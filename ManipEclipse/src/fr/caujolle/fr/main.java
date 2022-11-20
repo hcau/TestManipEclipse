0eadad5fe0d9a173d1a95269237511366fed0a01
hcau <herve.caujolle@gmail.com>
2022-11-20 18:44:37 +0100

    cas3
    cas4
    cas5

--- ManipEclipse/src/fr/caujolle/fr/main.java
+++ ManipEclipse/src/fr/caujolle/fr/main.java
@@ -5,7 +5,11 @@ public class main {
 	public main() {
 	 System.out.println("coucou");
 	 System.out.println("cas1");
-	 System.out.println("cas2");	 
+	 System.out.println("cas2");
+	 System.out.println("cas3");		
+	 System.out.println("cas4");
+	 System.out.println("cas5");
+	 System.out.println("cas6");		 
 	}
 	
 }
