74453c82d32710c4b5c42982b0dfd2af7920819a
hcau <herve.caujolle@gmail.com>
2022-11-20 18:40:41 +0100

    dd

---
+++ ManipEclipse/src/fr/caujolle/fr/main.java
@@ -0,0 +1,9 @@
+package fr.caujolle.fr;
+
+public class main {
+	
+	public main() {
+	 System.out.println("coucou");
+	}
+	
+}
---
+++ ManipEclipse/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hcau
+ *
+ */
+module ManipEclipse {
+}
